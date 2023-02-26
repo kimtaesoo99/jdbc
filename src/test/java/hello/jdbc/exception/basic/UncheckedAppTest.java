b03bc0cc2e75e812cacf0d76ffdf46aa85c1bd40
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-26 15:46:04 +0900

    test: 로그 추가 및 기존 예외 확인 테스트

--- src/test/java/hello/jdbc/exception/basic/UncheckedAppTest.java
+++ src/test/java/hello/jdbc/exception/basic/UncheckedAppTest.java
@@ -1,10 +1,12 @@
 package hello.jdbc.exception.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+@Slf4j
 public class UncheckedAppTest {
 
     @Test
@@ -13,6 +15,15 @@ public class UncheckedAppTest {
         Assertions.assertThatThrownBy(controller::request).isInstanceOf(RuntimeSQLException.class);
     }
 
+    @Test
+    public void printEx() {
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        }catch (Exception e){
+            log.info("ex",e);
+        }
+    }
 
     static class Controller{
         Service service = new Service();
@@ -59,6 +70,9 @@ public class UncheckedAppTest {
     }
 
     static class RuntimeSQLException extends RuntimeException{
+        public RuntimeSQLException() {
+        }
+
         public RuntimeSQLException(Throwable cause) {
             super(cause);
         }
