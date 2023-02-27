c94d997ede0cdf1b7e2fd76b84622f479e2c3426
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-27 21:23:09 +0900

    test: JdbcTemplate를 사용한 테스트

--- src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -2,7 +2,8 @@ package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepository;
-import hello.jdbc.repository.MemberRepositoryV4_1;
+import hello.jdbc.repository.MemberRepositoryV4_2;
+import hello.jdbc.repository.MemberRepositoryV5;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -21,6 +22,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
  * 예외 누수 문제 해결
  * SQLException 제거
  * MemberRepository 인터페이스 의존
+ *
+ * + SQLExceptionTranslator 추가
  */
 
 @Slf4j
@@ -47,8 +50,11 @@ class MemberServiceV4Test {
 
         @Bean
         MemberRepository memberRepositoryV4(){
-            return new MemberRepositoryV4_1(dataSource);
+//            return new MemberRepositoryV4_1(dataSource);
+//            return new MemberRepositoryV4_2(dataSource);
+            return new MemberRepositoryV5(dataSource);
         }
+
         @Bean
         MemberServiceV4 memberServiceV4(){
             return new MemberServiceV4(memberRepositoryV4());
